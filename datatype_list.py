9bd8ff0331fb5c16d9235ffd720fc3a63ad28c9f
AKAVespaET3 <quattrini244@gmail.com>
2023-08-01 18:22:52 +0900

    2023-08-01

---
+++ datatype_list.py
@@ -0,0 +1,21 @@
+#slice
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango", "peach"]
+type(thislist)
+# <class 'list'>
+# special variables
+# function variables
+len(thislist)
+# 8
+thislist[1:3]
+# ['banana', 'cherry']
+thislist[:-1]
+# ['apple', 'banana', 'cherry', 'orange', 'kiwi', 'melon', 'mango']
+
+# change value in list
+thislist[1]  = 'watermelon'
+# thislist[1:3] = ['cherry', 'watermelon'] 2번째 3번째 수정
+# thislist.sort() 오름차순
+# thislist.sort(reverse=True) 내림차순
+
+
+
